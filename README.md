ad41516fec1ec40952a2b26324e6afbe46fd5f4f
抖音牌轻薄本 <a15315862177@163.com>
2022-04-12 12:14:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# 神圣跨性别帝国护照生成器
+
+![img](example.png)
+
+
+## 安装依赖
+```bash
+pip3 install pillow qrcode
+```
+
+## 生成属于您的护照
+```bash
+python3 main.py
+```
+
+您的护照将会保存在`out.png`中
+
+## todo
++ ~~自定义头像~~ 您甚至可以直接更改`head.png`
+
+## license
+`main.py` 使用 MIT 协议
+
+`*.ttf`, `*.otf` 归原作者管
+
+`background.png`, `head.png`, `mask.png`, `EXAMPLE_STI.krz` 归原作者管
+
---
+++ main.py
@@ -0,0 +1,73 @@
+from PIL import Image, ImageDraw, ImageFont
+import random
+import qrcode
+import datetime
+
+
+def main():
+    name = input("您的英文名是啥? ")
+    eth_input = input("您的变态坊是? ")
+    if (eth_input.startswith("0x") or eth_input.startswith("0X")) and len(eth_input) == 42:
+        eth_addr = "0x" + eth_input[2:].upper()
+    elif len(eth_input) == 40:
+        eth_addr = "0x" + eth_input.upper()
+    else:
+        eth_addr = "0x" + \
+            "".join(random.choices("ABCDEF" + "1234567890", k=40))
+        print("计算机无法理解您的变态坊, 于是随机生成了:", eth_addr)
+    pron = input("您的代词是? ")
+    if pron.find("she") != -1:
+        pron = "ILLA/she"
+    elif pron.find("he") != -1:
+        pron = "ILLE/He"
+    else:
+        pron = "ILLVD/They"
+    date_born_input = input("您的出生日期是?(格式: 2022.1.1) ")
+    date_born_parse = datetime.datetime.strptime(date_born_input, "%Y.%m.%d")
+    date_born = date_born_parse.strftime("%Y / %m %b / %d").upper()
+    place = input("您在何处出生? ")
+    
+    id_1 = "P<TGD" + name.replace(" ", "<")
+    id_1 += "<" * (44 - len(id_1))
+    id_2 = eth_addr + "<" * 2
+    qr = qrcode.QRCode(version=1,
+                       box_size=4,
+                       border=0)
+    qr.add_data(eth_addr)
+    qr_img = qr.make_image(fill_color=(0, 0, 0),
+                           back_color="transparent")
+    qr.make(fit=True)
+    date_now = datetime.datetime.now().strftime("%Y / %m %b / %d").upper()
+    
+    f_number = ImageFont.truetype("data-latin.ttf", size=60)
+    f_text = ImageFont.truetype("SourceSansPro-Semibold.otf", size=35)
+    f_id = ImageFont.truetype(
+        "JetBrainsMono-Regular.ttf", size=50, layout_engine=ImageFont.LAYOUT_BASIC)
+    im = Image.open("background.png")
+    mask = Image.open("mask.png")
+    head = Image.open("head.png")  # 照片
+
+    imd = ImageDraw.ImageDraw(im)
+    imd.text((1147, 175), eth_addr[2:6] + eth_addr[38:42], font=f_number,
+             fill=(244, 62, 66))  # BTH地址首末
+    imd.text((500, 292), name, font=f_text, fill=(0, 0, 0))  # 名字
+    imd.text((1004, 292), pron, font=f_text, fill=(0, 0, 0))  # 代词
+    imd.text((500, 510), date_born,
+             font=f_text, fill=(0, 0, 0))  # 出生日期
+    imd.text((1004, 510), place, font=f_text, fill=(0, 0, 0))  # 出生地点
+    imd.text((500, 615), date_now,
+             font=f_text, fill=(0, 0, 0))  # 签发日期
+    imd.text((88, 794), id_1,
+             font=f_id, fill=(0, 0, 0))  # 签发日期
+    imd.text((88, 874), id_2,
+             font=f_id, fill=(0, 0, 0))  # 签发日期
+
+    im.paste(qr_img, (1270, 20), mask=qr_img)
+    im.alpha_composite(head)
+    im.alpha_composite(mask)
+
+    im.save("out.png")
+
+
+if __name__ == "__main__":
+    main()
